e6a93e2951380e9bb51656ab82396a78c215501f
= <=>
2021-10-21 01:30:45 +0000

    ejercicio1

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,23 +1,15 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
-    'sap/m/MessageToast'
 ], 
 /**
  * 
  * @param {*} Controller 
- * @param {*} MessageToast 
+
  */
-function(Controller, MessageToast) {
+function(Controller) {
     return Controller.extend("logaligroup.sapui5.controller.App", {
         onInit: function(){            
             
         },
-        onShowHello: function() {
-            const oBundle = this.getView().getModel("i18n").getResourceBundle();
-            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-            //console.log("aqui estoy soy el console");
-            MessageToast.show(sMsg);
-        }
     });
 });
---
+++ webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,24 @@
+sap.ui.define([
+    'sap/ui/core/mvc/Controller',
+    'sap/m/MessageToast'
+], 
+/**
+ * 
+ * @param {typeof sap/ui/core/mvc/Controller} Controller 
+ * @param {typeof sap/m/MessageToast} MessageToast
+ */
+function(Controller, MessageToast) {
+    return Controller.extend("logaligroup.sapui5.controller.HelloPanel", {
+        onInit: function(){            
+            
+        },
+        onShowHello: function() {
+            const oBundle = this.getView().getModel("i18n").getResourceBundle();
+            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+            //console.log("aqui estoy soy el console");
+            MessageToast.show(sMsg);
+        }
+    });
+},
+);
