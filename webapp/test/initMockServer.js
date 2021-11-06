3a1d010c7dc5d4031c665973a2806980acbc27e2
= <=>
2021-11-06 15:10:32 +0000

    tarea mock server

---
+++ webapp/test/initMockServer.js
@@ -0,0 +1,12 @@
+//ts-nocheck
+sap.ui.define([
+    '../localService/mockserver',
+    'sap/m/MessageBox'
+], 
+/**
+ * param  { typeof sap.m.MessageBox }
+ */
+function(require, factory) {
+    'use strict';
+    
+});
